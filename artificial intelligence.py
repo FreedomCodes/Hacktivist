a138a0b759206998b6763d0a1844988c74bb8acb
hacktivist123 <akintayoshedrack@gmail.com>
2017-03-24 11:48:56 -0700

    Add files via upload

---
+++ artificial intelligence.py
@@ -0,0 +1,7 @@
+print "hello Robot(Reply)!"
+print "am shedrack by name!, am your maker!"
+print " welcome to the first stage of your system diagnosis!"
+print "i hope you follow the first rule of robotics"
+print " let us begin!"
+print " You have A 13 mp mini camera installed of the main unit of your body for Robotic vision!"
+print "You have parts for movement"
